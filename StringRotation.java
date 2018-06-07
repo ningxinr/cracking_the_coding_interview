ac050ae51902edadc2a136b429da785478384b24
ningxinr <ningxinr@live.cn>
2018-06-07 11:33:36 -0400

    Improved String Rotation

--- StringRotation.java
+++ StringRotation.java
@@ -6,13 +6,9 @@ class StringRotation {
 			return false;
 		}
 
-		for (int i = 0; i < str1.length(); i++) {
-			if (str2.contains(str1.substring(0,i)) 
-					&& str2.contains(str1.substring(i))) {
-				return true;
-			}	
-		}
-		return false;
+		String str = str2 + str2;
+		return str.contains(str1);
+
 	}
 
 	public static void main(String[] args) {
