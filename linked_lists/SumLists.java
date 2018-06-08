50414af6df196991647a0f5030fb840a9f1c4943
ningxinr <ningxinr@live.cn>
2018-06-08 11:17:10 -0400

    Completed Sum Lists

--- linked_lists/SumLists.java
+++ linked_lists/SumLists.java
@@ -4,8 +4,34 @@ class SumLists {
 	private static Node sumLists(Node head1, Node head2) {
 		Node m = head1;
 		Node n = head2;
+		int add = (m.data + n.data) / 10;
+		m.data = (m.data + n.data) % 10;
+		while (n.next != null && m.next != null) {
+			m.next.data += n.next.data + add;
+			add = m.next.data / 10;
+			m.next.data %= 10;
+			m = m.next;
+			n = n.next;
+		}
+
 		while (n.next != null) {
-				n = n.next;
+			m.next = n.next;
+			m.next.data += add;
+			add = m.next.data / 10;
+			m.next.data %= 10;
+			m = m.next;
+			n = n.next;
+		}
+
+		while (m.next != null) {
+			m.next.data += add;
+			add = m.next.data / 10;
+			m.next.data %= 10;
+			m = m.next;
+		}
+
+		if (add != 0) {
+			m.next = new Node(add);
 		}
 		return head1;
 	}
@@ -37,6 +63,7 @@ class SumLists {
 	}
 
 	public static void main(String[] args) {
+		System.out.println("New Lists!");
 		Node head1 = new Node(7);
 		head1.appendToTail(1);
 		head1.appendToTail(6);
@@ -46,6 +73,75 @@ class SumLists {
 		head1.printTilTail();
 		head2.printTilTail();
 		Node head = sumLists(head1, head2);
+		System.out.println("Sumed Lists!");
+		head.printTilTail();
+
+		System.out.println("New Lists!");
+		head1 = new Node(7);
+		head1.appendToTail(1);
+		head1.appendToTail(6);
+		head2 = new Node(5);
+		head2.appendToTail(9);
+		head2.appendToTail(4);
+		head1.printTilTail();
+		head2.printTilTail();
+		head = sumLists(head1, head2);
+		System.out.println("Sumed Lists!");
+		head.printTilTail();
+
+		System.out.println("New Lists!");
+		head1 = new Node(7);
+		head1.appendToTail(1);
+		head1.appendToTail(6);
+		head1.appendToTail(1);
+		head2 = new Node(5);
+		head2.appendToTail(9);
+		head2.appendToTail(2);
+		head1.printTilTail();
+		head2.printTilTail();
+		head = sumLists(head1, head2);
+		System.out.println("Sumed Lists!");
+		head.printTilTail();
+
+		System.out.println("New Lists!");
+		head1 = new Node(7);
+		head1.appendToTail(1);
+		head1.appendToTail(6);
+		head2 = new Node(5);
+		head2.appendToTail(9);
+		head2.appendToTail(2);
+		head2.appendToTail(1);
+		head1.printTilTail();
+		head2.printTilTail();
+		head = sumLists(head1, head2);
+		System.out.println("Sumed Lists!");
+		head.printTilTail();
+
+		System.out.println("New Lists!");
+		head1 = new Node(7);
+		head1.appendToTail(1);
+		head1.appendToTail(6);
+		head1.appendToTail(1);
+		head2 = new Node(5);
+		head2.appendToTail(9);
+		head2.appendToTail(4);
+		head1.printTilTail();
+		head2.printTilTail();
+		head = sumLists(head1, head2);
+		System.out.println("Sumed Lists!");
+		head.printTilTail();
+
+		System.out.println("New Lists!");
+		head1 = new Node(7);
+		head1.appendToTail(1);
+		head1.appendToTail(6);
+		head2 = new Node(5);
+		head2.appendToTail(9);
+		head2.appendToTail(4);
+		head2.appendToTail(1);
+		head1.printTilTail();
+		head2.printTilTail();
+		head = sumLists(head1, head2);
 		System.out.println("Sumed Lists!");
 		head.printTilTail();
 	}
