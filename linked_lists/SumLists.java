0efd56115ff9758c7d62a4d9bbe691f5d89e9a33
ningxinr <ningxinr@live.cn>
2018-06-08 12:56:07 -0400

    Completed Sum Reverse Lists

--- linked_lists/SumLists.java
+++ linked_lists/SumLists.java
@@ -39,10 +39,60 @@ class SumLists {
 	private static Node sumListsR(Node head1, Node head2) {
 		Node m = head1;
 		Node n = head2;
-		while (n.next != null) {
-				n = n.next;
+		while (m.next != null && n.next != null) {
+			m = m.next;
+			n = n.next;
 		}
-		return head1;
+		
+		m = findStart(head1, m);
+		n = findStart(head2, n);
+
+		Node base = (m == head1) ? n : m;
+		Node adder = (m == head1) ? m : n;
+
+		base.data += adder.data;
+
+		while (base.next != null && adder.next != null) {
+			base.next.data += adder.next.data;
+			base = base.next;
+			adder = adder.next;
+		}
+			
+		Node baseHead = (m == head1) ? head2 : head1;
+		boolean invalid = true;
+
+		while (invalid) {
+			base = baseHead;
+			invalid = base.data > 10;
+			if (invalid) {
+				Node newHead = new Node(baseHead.data / 10);
+				baseHead.data %= 10;
+				newHead.next = baseHead;
+				baseHead = newHead;
+			}	
+
+			while (base.next != null) {
+				invalid |= base.next.data > 10;
+				base.data += base.next.data / 10;
+				base.next.data %= 10;
+				base = base.next;
+			}
+		}
+
+		return baseHead;
+	}
+
+	private static Node findStart(Node head, Node n) {
+		if (n.next == null) {
+			return head;
+		}
+
+		Node temp = head;
+		while (n.next != null) {
+			temp = temp.next;
+			n = n.next;
+		}
+		return temp;
 	}
 
 	private static Node deleteAllNodes(Node head, int d) {
@@ -72,7 +122,7 @@ class SumLists {
 		head2.appendToTail(2);
 		head1.printTilTail();
 		head2.printTilTail();
-		Node head = sumLists(head1, head2);
+		Node head = sumListsR(head1, head2);
 		System.out.println("Sumed Lists!");
 		head.printTilTail();
 
@@ -85,7 +135,7 @@ class SumLists {
 		head2.appendToTail(4);
 		head1.printTilTail();
 		head2.printTilTail();
-		head = sumLists(head1, head2);
+		head = sumListsR(head1, head2);
 		System.out.println("Sumed Lists!");
 		head.printTilTail();
 
@@ -99,7 +149,7 @@ class SumLists {
 		head2.appendToTail(2);
 		head1.printTilTail();
 		head2.printTilTail();
-		head = sumLists(head1, head2);
+		head = sumListsR(head1, head2);
 		System.out.println("Sumed Lists!");
 		head.printTilTail();
 
@@ -113,7 +163,7 @@ class SumLists {
 		head2.appendToTail(1);
 		head1.printTilTail();
 		head2.printTilTail();
-		head = sumLists(head1, head2);
+		head = sumListsR(head1, head2);
 		System.out.println("Sumed Lists!");
 		head.printTilTail();
 
@@ -127,7 +177,7 @@ class SumLists {
 		head2.appendToTail(4);
 		head1.printTilTail();
 		head2.printTilTail();
-		head = sumLists(head1, head2);
+		head = sumListsR(head1, head2);
 		System.out.println("Sumed Lists!");
 		head.printTilTail();
 
@@ -141,7 +191,7 @@ class SumLists {
 		head2.appendToTail(1);
 		head1.printTilTail();
 		head2.printTilTail();
-		head = sumLists(head1, head2);
+		head = sumListsR(head1, head2);
 		System.out.println("Sumed Lists!");
 		head.printTilTail();
 	}
